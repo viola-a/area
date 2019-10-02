83f16b1e71bd50c557d44a18648bdef8947933e3
viola <viola.asamoeh.2023@student.lhschools.org>
2019-10-02 08:38:20 -0400

    init scanner and wrote prompts

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,23 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
 
 //TODO DECLARE VARIABLES
-
+int base;
+int height;
+int area;
+String answer;
+Scanner lighthouse;
 //TODO INITIALIZE VARIABLES
-
+lighthouse=new Scanner(System.in);
 //TODO PROMOTE THE USER
+System.out.println("welcome to the area caculator");
+System.out.println("do you want to find the are of a \"square\" or \"rectangle\"?");
 
 //TODO BUILD VARIABLES FOR EQUATION
 
