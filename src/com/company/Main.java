75f6d7d6449a7ad4af25873d9a0aee43bf46768b
viola <viola.asamoeh.2023@student.lhschools.org>
2019-10-02 08:25:32 -0400

    WROTE PLAN

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,23 +5,17 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
+//TODO DECLARE VARIABLES
 
+//TODO INITIALIZE VARIABLES
 
+//TODO PROMOTE THE USER
 
+//TODO BUILD VARIABLES FOR EQUATION
 
+//TODO CACULATE
 
-
-
-
-
-
-
-
-
-
-
-
-
+//TODO PRINT RESULTS
 
 
 
