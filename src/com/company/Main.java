23de9db00f6570c99a4509fbcd802f95e3812397
viola <viola.asamoeh.2023@student.lhschools.org>
2019-10-02 08:57:23 -0400

    preformed caculations printed results

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,14 +18,29 @@ lighthouse=new Scanner(System.in);
 //TODO PROMOTE THE USER
 System.out.println("welcome to the area caculator");
 System.out.println("do you want to find the are of a \"square\" or \"rectangle\"?");
-
 //TODO BUILD VARIABLES FOR EQUATION
+answer = lighthouse.nextLine();
+
+if(answer.equals("sqaure")) {
+    System.out.println("what is the lenth of onr side?");
+    base=lighthouse.nextInt();
+    area = base * base;
+}
+
+else{
+    System.out.println("what is the lenth of the base ");
+    base=lighthouse.nextInt();
+    System.out.println("what is the height of the base ");
+    height=lighthouse.nextInt();
+    area=base*height;
+
+}
+
 
 //TODO CACULATE
 
 //TODO PRINT RESULTS
-
-
+System.out.println("the area of the"+ answer + " is "+area);
 
 
 
